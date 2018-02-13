32e8d9d6bf68a6d85db52b57b0743fea326a1cbf
Mitchell Lewis <mblewis@umail.ucsb.edu>
2018-02-12 20:32:51 -0800

    Added filename-finding, moving onto text parsing

--- main.cpp
+++ main.cpp
@@ -1,35 +1,73 @@
 // main.cpp - Testing and Time-checking of BST and Hash Table implementations
 // Mitchell Lewis, Sean Costello
 
+#include <sys/types.h>
+#include <dirent.h>
+#include <vector>
 #include <string>
 #include <chrono>
+#include <fstream>
 #include <sstream>
 #include <iostream>
 #include "HashTable.h"
 
 using namespace std;
 
+// Read_directory - writes filenames inside a given directory to a string vector
+void read_directory(const string& name, vector<string>* v){
+  DIR* dirp = opendir(name.c_str());
+  struct dirent * dp;
+  while((dp = readdir(dirp)) != NULL){
+    // adds name of entry to list as long as it does not start with a '.'
+    if((dp->d_name)[0] != '.'){
+      v->push_back(dp->d_name);
+    }
+  }
+  closedir(dirp);
+}
+
+void parse_text(const string& name, vector<string>* v){
+  ifstream inFile(name);
+  string str;
+
+  while(inFile >> str){
+    
+
+    //v->push_back(str);
+  }
+
+  inFile.close();
+}
+
 int main(){
-  /*
-  auto start = std::chrono::high_resolution_clock::now();
-               any function we want here
-  auto stop = std::chrono::high_resolution_clock::now();
 
-  std::chrono::duration<double, std::milli> execTime = stop - start;
-  */
+  //Text parsing
+  vector<string> filenames, dirnames;
+  string root = "hotels-small";
 
-  HashTable* ht = new HashTable(100);
-  ht->insert("abc");
-  ht->insert("dog");
-  ht->insert("god");
-  ht->insert("airplane");
-  ht->insert("grapes");
-  ht->insert("monorail");
-  ht->insert("get");
-  ht->insert("xylophone");
-  ht->insert("words");
-  ht->insert("airplane");
+  // retrieves subdirectory names
+  read_directory(root, &dirnames);
+
+  // retrieves filenames inside each subdirectory, then builds the path up to the main dataset folder, then adds
+  // to filename list
+  for(unsigned int i=0; i<dirnames.size(); i++){
+    vector<string> tempFileNames;
+    string subdir = root + "/" + dirnames.at(i);
+    read_directory(subdir, &tempFileNames);
+    for(unsigned int j=0; j<tempFileNames.size(); j++){
+      
+      string path = subdir + "/" + tempFileNames.at(j);
+      filenames.push_back(path);
+    }
+  }
+
+  vector<string> rawData;
+  parse_text(filenames.at(0), &rawData);
+
   
+  HashTable* ht = new HashTable(10);
+
+  // Test interface - Prompts input for 1-5, performs search, insert, remove, sort, and range search respectively
   while(true){
     ostringstream out;
     int choice;
@@ -68,7 +106,7 @@ int main(){
 	  break;
 	}
 
-      //delete
+      //remove
       case 3:
 	{
 	  cin >> w;
@@ -111,6 +149,7 @@ int main(){
     default:
       {
 	cin.clear();
+	cin.ignore(10000, '\n');
 	cout << "Invalid input" << endl;
       }
     }
@@ -119,4 +158,5 @@ int main(){
   
   return 0;
 }
+
 
